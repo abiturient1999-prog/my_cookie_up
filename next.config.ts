d33b303da3e05348b07c81c92f69e1922b886337
abiturient1999-prog <abiturient1999@gmail.com>
2026-02-13 21:43:31 +0300

    122

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@react-native-async-storage/async-storage": false,
+    };
+
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
